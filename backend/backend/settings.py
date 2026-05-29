d4b2a6e54e8cfb57437cd329453c68760d755b95
steve-ongera <gadafisteve001@gmail.com>
2026-05-29 17:46:06 +0300

    made updates

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'corsheaders.middleware.CorsMiddleware',          # Must be before CommonMiddleware
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
