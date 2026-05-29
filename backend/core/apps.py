a0f211339d2926da9cbc1405af2682b53fa9ec6a
steve-ongera <gadafisteve001@gmail.com>
2026-05-29 20:32:50 +0300

    made updates

--- backend/core/apps.py
+++ backend/core/apps.py
@@ -1,6 +1,19 @@
+# ─────────────────────────────────────────────────────────────────────────────
+# core/apps.py
+# ─────────────────────────────────────────────────────────────────────────────
+ 
+"""
+core/apps.py
+"""
+ 
 from django.apps import AppConfig
-
-
-class CoreConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'core'
+ 
+ 
+class ExaminationsConfig(AppConfig):
+    name            = 'apps.examinations'
+    verbose_name    = 'KCSE Examinations'
+    default_auto_field = 'django.db.models.UUIDField'
+ 
+    def ready(self):
+        # Connect signals when the app is loaded
+        import core.signals  # noqa: F401
